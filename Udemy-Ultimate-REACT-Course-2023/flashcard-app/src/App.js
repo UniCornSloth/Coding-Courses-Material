3786280b5bfed43db4e5740ce36940c90ca8e44b
Herling <bothaherling@gmail.com>
2023-07-06 15:38:46 +0200

    update

--- Udemy-Ultimate-REACT-Course-2023/flashcard-app/src/App.js
+++ Udemy-Ultimate-REACT-Course-2023/flashcard-app/src/App.js
@@ -44,13 +44,18 @@ const questions = [
 ];
 
 function FlashCards() {
-const [selecedId, setSelectedId] = useState(null)
+const [selectedId, setSelectedId] = useState(null)
 
+function handleClick(id) {
+  setSelectedId(id !== selectedId ? id : null)
+}
   return (
   <div className="flashcards">
     {questions.map((question) => (
-    <div key={question.id}>
-      <p>{question.question}</p>
+    <div key={question.id}
+    onClick={() => handleClick(question.id)}
+    className={question.id === selectedId ? "selected" : ""}>
+      <p>{question.id === selectedId ? question.answer : question.question}</p>
     </div>
     ))}    
   </div>
